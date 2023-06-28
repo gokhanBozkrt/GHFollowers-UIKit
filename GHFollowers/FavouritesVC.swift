e41fa0cd1bd665ee0511d81b35fc74406fe57170
Gökhan Bozkurt <99559446+gokhanBozkrt@users.noreply.github.com>
2023-06-28 08:46:18 +0300

    Initial Commit

---
+++ GHFollowers/FavouritesVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavouritesVC.swift
+//  GHFollowers
+//
+//  Created by Gökhan Bozkurt on 27.06.2023.
+//
+
+import UIKit
+
+class FavouritesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+}
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Gökhan Bozkurt on 27.06.2023.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+    }
+
+
+}
