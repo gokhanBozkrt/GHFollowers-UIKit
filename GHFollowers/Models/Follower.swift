e8651832da853a9f255b8174053ed80a63ec4d7d
Gökhan Bozkurt <99559446+gokhanBozkrt@users.noreply.github.com>
2023-07-08 22:41:17 +0300

    - Add network manager

---
+++ GHFollowers/Models/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Gökhan Bozkurt on 8.07.2023.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
