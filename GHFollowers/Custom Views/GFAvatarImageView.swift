acb82587fc072bde479379a538acacaa791a5bc6
Gökhan Bozkurt <99559446+gokhanBozkrt@users.noreply.github.com>
2023-07-09 12:43:13 +0300

    - Add collection view Custom follower cell

---
+++ GHFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,30 @@
+//
+//  GFAvatarImageView.swift
+//  GHFollowers
+//
+//  Created by Gökhan Bozkurt on 9.07.2023.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+
+    let placeHolderImage = UIImage(named: "avatar-placeholder")!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeHolderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
