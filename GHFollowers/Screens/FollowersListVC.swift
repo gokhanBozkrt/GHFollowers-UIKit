96ae927a5941291888be68003ff62da4fabbab8f
Gökhan Bozkurt <99559446+gokhanBozkrt@users.noreply.github.com>
2023-07-09 13:19:41 +0300

    - Do  UICollectionView  initial setup

--- GHFollowers/Screens/FollowersListVC.swift
+++ GHFollowers/Screens/FollowersListVC.swift
@@ -9,27 +9,44 @@ import UIKit
 
 class FollowersListVC: UIViewController {
     var userName: String?
+    var collectionView: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        navigationController?.navigationBar.prefersLargeTitles = true
-        
-  
-        
-        NetworkManager.shared.getFollowers(for: userName ?? "", page: 1) { (result) in
-            switch result {
-            case .success(let followers):
-                print("Followers.count = \(followers.count)")
-                print(followers)
-            case .failure(let error):
-                self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "Ok")
-            }
-        }
+        configureViewController()
+        configureCollectionView()
+        getFollowers()
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    func configureViewController() {
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    func getFollowers() {
+               NetworkManager.shared.getFollowers(for: userName ?? "", page: 1) { (result) in
+                   switch result {
+                   case .success(let followers):
+                       print("Followers.count = \(followers.count)")
+                       print(followers)
+                   case .failure(let error):
+                       self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "Ok")
+                   }
+               }
     }
 }
 
