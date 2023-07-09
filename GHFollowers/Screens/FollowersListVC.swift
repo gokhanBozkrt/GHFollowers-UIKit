ccd515b4457c9ae8b665f668528eb830217649f1
Gökhan Bozkurt <99559446+gokhanBozkrt@users.noreply.github.com>
2023-07-09 09:15:21 +0300

    - Do network call with result type

--- GHFollowers/Screens/FollowersListVC.swift
+++ GHFollowers/Screens/FollowersListVC.swift
@@ -14,15 +14,16 @@ class FollowersListVC: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         
+  
         
-        NetworkManager.shared.getFollowers(for: userName ?? "", page: 1) { (followers, errorMessage) in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: errorMessage!.rawValue, buttonTitle: "Ok")
-                return
+        NetworkManager.shared.getFollowers(for: userName ?? "", page: 1) { (result) in
+            switch result {
+            case .success(let followers):
+                print("Followers.count = \(followers.count)")
+                print(followers)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "Ok")
             }
-            
-            print("Followers.count = \(followers.count)")
-            print(followers)
         }
     }
     
