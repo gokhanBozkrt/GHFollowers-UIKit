8b33f4d48f1810e0aebfe0d8c86d68c4c4b682e8
Gökhan Bozkurt <99559446+gokhanBozkrt@users.noreply.github.com>
2023-07-23 12:09:11 +0300

    - Weak self, arch memory leaks

---
+++ GHFollowers/Utilities/UIHelper.swift
@@ -0,0 +1,25 @@
+//
+//  UIHelper.swift
+//  GHFollowers
+//
+//  Created by Gökhan Bozkurt on 23.07.2023.
+//
+
+import UIKit
+
+struct UIHelper {
+    
+    static func createThreeColumnFlowLayout(in view: UIView) -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let avaliableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = avaliableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+}
