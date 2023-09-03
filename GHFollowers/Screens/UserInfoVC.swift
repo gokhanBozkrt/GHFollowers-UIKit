216c34f7737253f616400acab03b8f05b50b21af
Gökhan Bozkurt <99559446+gokhanBozkrt@users.noreply.github.com>
2023-09-03 22:45:56 +0300

    - Add user info header view vc

--- GHFollowers/Screens/UserInfoVC.swift
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -10,23 +10,46 @@ import UIKit
 class UserInfoVC: UIViewController {
 
     var userName: String!
+    let headerView = UIView()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done,target: self, action: #selector(dismissVC))
         navigationItem.rightBarButtonItem = doneButton
-      
+        layoutUI()
         NetworkManager.shared.getUserInfo(for: userName) { [weak self] result in
             guard let self = self else { return }
             switch result {
             case .success(let user):
-                print(user)
+                DispatchQueue.main.async {  
+                    self.add(childVC: GFUserInfoHeaderVC(user: user), to: self.headerView)
+                }
             case .failure(let failure):
                 self.presentGFAlertOnMainThread(title: "Something went wrong", message: failure.rawValue, buttonTitle: "Ok")
             }
         }
+        
+        
+    }
+    func add(childVC: UIViewController, to containerView: UIView) {
+        addChild(childVC)
+        containerView.addSubview(childVC.view)
+        childVC.view.frame = containerView.frame
+        childVC.didMove(toParent: self)
     }
     
+    func layoutUI() {
+        view.addSubview(headerView)
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 180)
+        ])
+        
+        
+    }
   @objc func dismissVC() {
         dismiss(animated: true)
     }
---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Gökhan Bozkurt on 3.09.2023.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
