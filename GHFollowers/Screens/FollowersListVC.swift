3c1c8f447c7ff678b04db4018c2f635db266d478
Gökhan Bozkurt <bozkurtgokhan92@icloud.com>
2023-12-24 17:25:51 +0300

    - Add Folloer to Favourites List

--- GHFollowers/Screens/FollowersListVC.swift
+++ GHFollowers/Screens/FollowersListVC.swift
@@ -54,7 +54,27 @@ class FollowersListVC: UIViewController {
     }
     
     @objc func addButtonTapped() {
-        
+        showLoadingView()
+        NetworkManager.shared.getUserInfo(for: userName ?? "") { [weak self ] result  in
+            guard let self = self else { return }
+            self.dismissLoadingView()
+            switch result {
+            case .success(let user):
+                let favourite = Follower(login: user.login, avatarUrl: user.avatarUrl)
+                PersistenceManager.updateWith(favourite: favourite, actionType: .add) { [weak self] error in
+                    guard let self = self else { return }
+                     
+                    guard let error = error else {
+                        self.presentGFAlertOnMainThread(title: "Success", message: "Follower Added your favourite list.", buttonTitle: "Ok")
+                        return
+                    }
+                    self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+
+                }
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
     }
     
     func configureCollectionView() {
